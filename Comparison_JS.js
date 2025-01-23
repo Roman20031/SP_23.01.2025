606c472abf35e86040c214ae54843d0218ddea9c
Roman20031 <>
2025-01-24 00:43:45 +0200

    SP_23.01.2025

---
+++ Comparison_JS.js
@@ -0,0 +1,13 @@
+
+function functionExample() {
+    console.log("This is an example of function in JavaScript");
+  }
+  
+functionExample();
+
+
+//                 Data types/Datove typy
+//   String: Textové řadky (např. "Dobrý den").
+//   Number: Čísla (např. 6, 19.86).
+//   Boolean: Pravdivostní hodnoty (true nebo false).
+                                                            //Roman Demianenko
---
+++ Comparison_Py.py
@@ -0,0 +1,13 @@
+
+def function_example():
+  print("This is an example of function in Python")
+
+function_example()
+
+#             Data types/Datove typy
+# str/string: Textové řadky (např. "Dobr7 den").
+# int/intenger: Celá čísla (např. 11).
+# float: Desetinná čísla (např. 20.06).
+# boolean: Pravdivostní hodnoty (True nebo False).
+
+#Roman Demianenko     Asi to vlastně je to co jsem měl za úkol z minule hodiny.(Vytvořít jakukoliv funkce a zkusit popsat datove typy)
